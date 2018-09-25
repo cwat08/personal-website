6cdd76d39c15a73e666a4caec7e9ef31f7f6ecef
Claire Watson <watson.e.claire@gmail.com>
2018-09-25 13:09:03 -0400

    fixed resume component, so its a live google doc

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,12 +6,26 @@ const projects = [
     title: 'Burst',
     description: 'Bipartisan web app to burst your filter bubble',
     image: './Mad_giphs_image.png'
+  },
+  {
+    title: 'MAD GIPHS',
+    description: 'find cool GIFs!',
+    image: './Mad_giphs_image.png'
+  },
+  {
+    title: 'Codebrary',
+    description: 'a site to buy coding books',
+    image: './Mad_giphs_image.png'
   }
 ]
 
 const ProjectList = props => {
-  return projects.map(project => {
-    return <SingleProject project={project} />
-  })
+  return (
+    <div className="project-list">
+      {projects.map(project => {
+        return <SingleProject project={project} />
+      })}
+    </div>
+  )
 }
 export default ProjectList
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
-const Resume = () => <img className="resume" src="./Watson_Claire_Resume.jpg" />
+const Resume = () => (
+  <div>
+    <p>
+      <iframe
+        // className="resume"
+        src="https://docs.google.com/document/d/e/2PACX-1vQQmuYhVYgC7Sgid3O-QTttPipF-UtX7NMqQtH3pZGxyMR5Bh8WTyqCQtayjJYpOWL1drPLywWhS_Wq/pub?embedded=true"
+        width="800"
+        height="1500"
+        scrolling="no"
+      />
+    </p>
+  </div>
+)
 
 export default Resume
