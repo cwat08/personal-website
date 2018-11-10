eaed5dffe83d979f833a2a23c1d8ad644f7faeb6
Claire Watson <watson.e.claire@gmail.com>
2018-11-09 23:11:18 -0500

    fixed size of footer text  for breakpoint

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,15 +1,9 @@
 import React from 'react'
 
 const Resume = () => (
-  <div id="resume">
-    <span className="fancy" />
+  <div id="resume" className="iframe-container">
     {/* <p> */}
-    <iframe
-      src="https://docs.google.com/document/d/e/2PACX-1vRAglJoNMgMBh57DayS7bW0lU-XtDAjgHqdXSa9QaZPlE9pKqsF6QqGZtWAD6g1p4f91Eb5ATPFzxAt/pub?embedded=true"
-      width="800"
-      height="1500"
-      scrolling="no"
-    />
+    <iframe src="https://docs.google.com/document/d/e/2PACX-1vRAglJoNMgMBh57DayS7bW0lU-XtDAjgHqdXSa9QaZPlE9pKqsF6QqGZtWAD6g1p4f91Eb5ATPFzxAt/pub?embedded=true" />
     {/* </p> */}
   </div>
 )
